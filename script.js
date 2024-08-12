9a105b2b536730a104b736a3e1c54c67e88b6610
Mukarramah319 <m-10685912@moe-dl.edu.my>
2024-08-12 15:46:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -49,9 +49,9 @@ function handleClick(e) {
 
 function endGame(draw) {
   if (draw) {
-    winningMessageTextElement.innerText = 'Draw!'
+    winningMessageTextElement.innerText = 'Seri!'
   } else {
-    winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`
+    winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Menang!`
   }
   winningMessageElement.classList.add('show')
 }
